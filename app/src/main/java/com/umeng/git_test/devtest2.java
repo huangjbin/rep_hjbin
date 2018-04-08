4538c2ec5f379268fa306b18b1df0d0db4b83594
huangjunbin <159357xx>
2018-04-07 17:37:58 -0700

    新增devtest2.java

---
+++ app/src/main/java/com/umeng/git_test/devtest2.java
@@ -0,0 +1,13 @@
+package com.umeng.git_test;
+/**
+ * Created by ${huangjunbin} on 18/4/7.
+ */
+
+/**
+ * Depiction：
+ * Author：huangjunbin
+ * Email：13794922596@163.com
+ * Date： 18/4/7 17:34
+ */
+public class devtest2 {
+}
